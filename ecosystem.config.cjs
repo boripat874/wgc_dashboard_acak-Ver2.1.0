c86b29a9e87304d1ad3d49fec83539a0e24e1260
boripat874 <boripat874@gmail.com>
2026-05-17 01:16:30 +0700

    ver2.1.0 17-05-2026

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
       name: "Dashboard WGC Acak",
       script: "npm",               // สคริปต์หลักที่สั่งรัน (หรือใส่เป็น "node_modules/next/dist/bin/next")
       args: "run start",           // อาร์กิวเมนต์ที่ส่งให้สคริปต์
-      instances: "3",            // จำนวนจำลองแอป: ใส่ตัวเลข หรือ "max" เพื่อใช้ CPU ให้ครบทุก Cores
+      instances: "1",            // จำนวนจำลองแอป: ใส่ตัวเลข หรือ "max" เพื่อใช้ CPU ให้ครบทุก Cores
       exec_mode: "cluster",        // โหมดการรัน: "fork" (รันเดี่ยว) หรือ "cluster" (ช่วยกระจายโหลด)
       env: {
         NODE_ENV: "production",
